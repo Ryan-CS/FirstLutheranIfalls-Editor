2a7f239bb09d9721a6f28dfb0e6ba813314e41a5
RY5K <ryan.skogstad@gmail.com>
2026-08-20 10:53:20 -0500

    Point editor at deployed Worker name

--- admin/config.js
+++ admin/config.js
@@ -1,5 +1,5 @@
 window.FLC_EDITOR_CONFIG = Object.freeze({
   environment: "firstlutheranifalls.site-test",
-  apiBase: "https://first-lutheran-editor-api.ryan-skogstad.workers.dev",
+  apiBase: "https://firstlutheranifalls-editor.ryan-skogstad.workers.dev",
   liveBase: "https://firstlutheranifalls.site"
 });
