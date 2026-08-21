e699acad92485e9832edf1101c8cecbe9495f276
RY5K <ryan.skogstad@gmail.com>
2026-08-20 19:34:24 -0500

    Publish editor runtime config from docs folder

---
+++ docs/config.js
@@ -0,0 +1,5 @@
+window.FLC_EDITOR_CONFIG = Object.freeze({
+  environment: "firstlutheranifalls.site-test",
+  apiBase: "https://firstlutheranifalls-editor.ryan-skogstad.workers.dev",
+  liveBase: "https://firstlutheranifalls.site"
+});
